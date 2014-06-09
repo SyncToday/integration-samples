84ea5152d66318cbea927be163bc211c4b2edbae
davidpodhola <david.podhola@naseukoly.cz>
2014-06-09 13:54:41 +0200

    initial commit DynamicsNAV71

---
+++ DynamicsNAV71SOAP/ConsoleApplication4/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApplication4.WebService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // http://msdn.microsoft.com/en-us/library/dd355316(v=nav.70).aspx
+            ServiceTasks_Binding client = new ServiceTasks_Binding();
+            client.UseDefaultCredentials = true;
+            ServiceTasks[] result = client.ReadMultiple(new ServiceTasks_Filter[] { }, "", 1000);
+            foreach (ServiceTasks task in result)
+                Console.WriteLine(task.Description);
+            Console.ReadLine();
+        }
+    }
+}
